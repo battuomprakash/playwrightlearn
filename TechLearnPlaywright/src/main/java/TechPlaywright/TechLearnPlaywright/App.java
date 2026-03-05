b8fb14097a4ea0be686e6266cb4414b7b1086106
battuomprakash <batuomprakash@gmail.com>
2026-03-05 13:01:02 +0530

    adding project

---
+++ TechLearnPlaywright/src/main/java/TechPlaywright/TechLearnPlaywright/App.java
@@ -0,0 +1,10 @@
+package TechPlaywright.TechLearnPlaywright;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ TechLearnPlaywright/src/test/java/playWrightTestingLearn/DemoPW.java
@@ -0,0 +1,34 @@
+package playWrightTestingLearn;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class DemoPW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Create Playwright instance
+        Playwright playwright = Playwright.create();
+
+        // Launch browser
+        Browser browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false));
+
+        // Create new page
+        Page page = browser.newPage();
+
+        // Navigate to website
+        page.navigate("https://www.google.com");
+
+        // Print page title
+        System.out.println("Page Title is: " + page.title());
+
+        // Close browser
+     //   browser.close();
+     //   playwright.close();
+	}
+
+}
